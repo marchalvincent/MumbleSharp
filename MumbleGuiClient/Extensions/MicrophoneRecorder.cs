68de626a048a8922f80313bce15e35c901557950
Vincent Marchal <vincent.marchal@avanade.com>
2024-06-13 10:54:35 +0200

    register user speech on disk

--- MumbleGuiClient/Extensions/MicrophoneRecorder.cs
+++ MumbleGuiClient/Extensions/MicrophoneRecorder.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using MumbleSharp;
 using NAudio.Wave;
 
@@ -6,6 +8,11 @@ namespace MumbleGuiClient
 {
     public class MicrophoneRecorder
     {
+        private readonly string _username;
+        private readonly string _recordingFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "records");
+        private MemoryStream _stream;
+        private DateTime _speechStartedAt;
+
         private readonly IMumbleProtocol _protocol;
 
         public bool _recording = false;
@@ -31,8 +38,12 @@ namespace MumbleGuiClient
 
         public MicrophoneRecorder(IMumbleProtocol protocol)
         {
-            VoiceDetectionThreshold = 0.5f;
+            VoiceDetectionThreshold = 0.2f;
             _protocol = protocol;
+
+            _username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Replace(" ", "").Replace("\\", "_");
+            if (!Directory.Exists(_recordingFolder))
+                Directory.CreateDirectory(_recordingFolder);
         }
 
         private void VoiceDataAvailable(object sender, WaveInEventArgs e)
@@ -40,6 +51,19 @@ namespace MumbleGuiClient
             if (!_recording)
                 return;
 
+
+            if (_stream != null)
+            {
+                // NB1: recording the speech on disk must be out of the "VoiceDetected if" below because this condition cuts the time when user doesn't talk. It is used to avoid sending useless "silent data" to the server
+                // NB2: the condition below (_protocol.LocalUser.Channel != null) can be used here if we want to record only when the gui is connected to a server
+                //if (_protocol.LocalUser != null && _protocol.LocalUser.Channel != null)
+                //{
+                    var arraySegment = new ArraySegment<byte>(e.Buffer, 0, e.BytesRecorded);
+                    _stream.Write(arraySegment);
+                //}
+            }
+
+            // Is the user talking?
             if (voiceDetector.VoiceDetected(new WaveBuffer(e.Buffer), e.BytesRecorded))
             {
                 //At the moment we're sending *from* the local user, this is kinda stupid.
@@ -52,6 +76,7 @@ namespace MumbleGuiClient
                 //if (_protocol.LocalUser != null)
                 //    _protocol.LocalUser.SendVoice(new ArraySegment<byte>(e.Buffer, 0, e.BytesRecorded));
 
+                // Is the mumble gui client connected to a server?
                 //Send to the channel LocalUser is currently in
                 if (_protocol.LocalUser != null && _protocol.LocalUser.Channel != null)
                 {
@@ -84,6 +109,10 @@ namespace MumbleGuiClient
             sourceStream.DataAvailable += VoiceDataAvailable;
 
             sourceStream.StartRecording();
+
+            // init stream for getting user speech in memory
+            _stream = new MemoryStream();
+            _speechStartedAt = DateTime.Now;
         }
 
         public void Stop()
@@ -92,6 +121,18 @@ namespace MumbleGuiClient
             _protocol.LocalUser?.Channel.SendVoiceStop();
 
             sourceStream.StopRecording();
+
+            // get filename regarding username, start date and duration of the speech
+            var speechDuration = DateTime.Now - _speechStartedAt;
+            string filename = Path.Combine(_recordingFolder, $"{_speechStartedAt.ToString("HH.mm.ss")}-{_username}-{(int)speechDuration.TotalSeconds}s.wav");
+
+            // write on disk user speech from the memory stream
+            _stream.Position = 0;
+            IWaveProvider provider = new RawSourceWaveStream(_stream, sourceStream.WaveFormat);
+            WaveFileWriter.CreateWaveFile(filename, provider);
+            _stream.Dispose();
+            _stream = null;
+
             sourceStream.Dispose();
             sourceStream = null;
         }
